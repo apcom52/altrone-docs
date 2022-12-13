787ead028d4cab57b27d769d5a9ba68fbdad269a
Alexander Perevezentsev <perevezentsev@innoscripta.com>
2022-12-13 10:41:56 +0300

    corners page

--- components/ImageFigure/ImageFigure.tsx
+++ components/ImageFigure/ImageFigure.tsx
@@ -11,7 +11,10 @@ interface ImageFigureProps {
 
 export const ImageFigure = ({ src, width, height, alt, children }: PropsWithChildren<ImageFigureProps>) => {
   return <figure className={s.ImageFigure}>
-    <Image src={src} alt={alt || ''} width={width} height={height} className={s.ImageFigure__image} />
+    <Image src={src} alt={alt || ''} width={width * 4} height={height * 4} className={s.ImageFigure__image} style={{
+      width,
+      height
+    }} />
     <figcaption className={s.ImageFigure__caption}>{children}</figcaption>
   </figure>
 }
---
+++ pages/design-system/corners.tsx
@@ -0,0 +1,44 @@
+import s from "../../styles/Home.module.css";
+import {Button, ButtonVariant, Heading, Icon, Paragraph, Role} from "../../altrone";
+import React from "react";
+import {ImageList} from "../../components/ImageFigure/ImageList";
+import Image from "next/image";
+import {ImageFigure} from "../../components/ImageFigure/ImageFigure";
+
+export default function Home() {
+  return (<>
+    <div className={s.backButton}>
+      <Button href='/design-system' variant={ButtonVariant.text} leftIcon={<Icon i='arrow_back_ios' />} role={Role.primary}>Design System</Button>
+      <Button variant={ButtonVariant.text} rightIcon={<Icon i='share' />} role={Role.primary}>Share</Button>
+    </div>
+    <Heading>
+      Rounded corners
+    </Heading>
+    <Paragraph>
+      There are very few objects in nature with sharp edges. So in Altrone all components have rounded corners. We use
+      three different radius: for containers (12px), controls (8px) and inner elements (6px). When you develop your own
+      components try to follow this values. This will allow you to make component that don't look like alien.
+    </Paragraph>
+    <ImageList>
+      <ImageFigure src='/images/design-system/corners1.png' alt='Three different values of corners' width={648} height={280}>
+        Three different values of corners: for containers, for controls and inner elements
+      </ImageFigure>
+    </ImageList>
+
+    <Paragraph>
+      Let's look on some example of usage this corner system:
+    </Paragraph>
+
+    <ImageList>
+      <ImageFigure src='/images/design-system/corners2.png' alt='Example with modal' width={259} height={140}>
+        Modal container has 12px radius, but button has 8px radius
+      </ImageFigure>
+      <ImageFigure src='/images/design-system/corners3.png' alt='Example with modal' width={252} height={140}>
+        ContextMenu has 12px radius, but selected ContextMenuItem has 8px radius
+      </ImageFigure>
+      <ImageFigure src='/images/design-system/corners4.png' alt='Example with modal' width={374} height={140}>
+        NumberInput has 8px radius, but counter has 6px radius
+      </ImageFigure>
+    </ImageList>
+  </>)
+}
